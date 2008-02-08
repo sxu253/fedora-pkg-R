bf8154d26eed1325b8f85a372c2baedb8f79d205
Tom Callaway <spot@fedoraproject.org>
2008-02-08 19:27:58 +0000

    2.6.2, take package cleanups to all active releases

--- R-make-search-index.sh
+++ R-make-search-index.sh
@@ -3,9 +3,13 @@
 # Figure out what RHOME is set to
 TMP_R_HOME=`R RHOME`
 
+# Figure out what R_DOC_DIR is set to
+# Ideally, we could ask R just like we do for RHOME, but we can't yet.
+TMP_R_DOC_DIR=`grep "R_DOC_DIR=" /usr/bin/R | cut -d "=" -f 2`
+
 # Write out all the contents in arch and noarch library locations
-cat $TMP_R_HOME/library/*/CONTENTS > $TMP_R_HOME/doc/html/search/index.txt 2>/dev/null
-cat /usr/share/R/library/*/CONTENTS >> $TMP_R_HOME/doc/html/search/index.txt 2>/dev/null
+cat $TMP_R_HOME/library/*/CONTENTS > $TMP_R_DOC_DIR/html/search/index.txt 2>/dev/null
+cat /usr/share/R/library/*/CONTENTS >> $TMP_R_DOC_DIR/html/search/index.txt 2>/dev/null
 
 exit 0
 
